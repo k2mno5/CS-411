cf96884549e6e761baa2bf78747f91e16129a28c
Luo <gluo2@fa17-cs411-44.cs.illinois.edu>
2017-10-31 16:00:35 -0500

    Tested dummy test cases

--- utilities/tests.py
+++ utilities/tests.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.test import TestCase
 
 # Create your tests here.
+class UtilitiesTestCase(TestCase):
+    def setUp(self):
+        self.dummyTest = 1
+    def test_dummy_test(self):
+        self.assertEqual(self.dummyTest, 1)
+    def test_dummy_test_2(self):
+        self.assertEqual(self.dummyTest, 0)
