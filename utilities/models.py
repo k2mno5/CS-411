18aa663ddaff395d2f356ba81cfb7949762c26a5
Luo <gluo2@fa17-cs411-44.cs.illinois.edu>
2017-10-31 21:01:32 -0500

    deploying dummy test for model

--- utilities/models.py
+++ utilities/models.py
@@ -4,3 +4,5 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Dummy(models.Model):
+    value = models.IntegerField(blank=True, null=True)
--- utilities/tests.py
+++ utilities/tests.py
@@ -2,12 +2,18 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from models import Dummy
 
 # Create your tests here.
 class UtilitiesTestCase(TestCase):
     def setUp(self):
         self.dummyTest = 1
+        fakeDummy = Dummy(value = 1)
+        fakeDummy.save()
+
     def test_dummy_test(self):
         self.assertEqual(self.dummyTest, 1)
-    def test_dummy_test_2(self):
-        self.assertEqual(self.dummyTest, 0)
+
+    def test_dummy_model(self):
+        dummy = Dummy.objects.get(pk=1)
+        self.assertEqual(dummy.value, 1)
